7e12445e98e084954fa631e7bfa373577206cb05
sarychev <dizel3d@gmail.com>
2015-11-15 12:40:31 +0300

    Добавил скрипт автоматической установки сертификатов.

---
+++ regcerts.cmd
@@ -0,0 +1,1 @@
+powershell -noprofile -executionpolicy bypass -file .\regcerts.ps1
---
+++ regcerts.ps1
@@ -0,0 +1,14 @@
+﻿$X509Store = [System.Security.Cryptography.X509Certificates.X509Store]
+$StoreName = [System.Security.Cryptography.X509Certificates.StoreName]
+$StoreLocation = [System.Security.Cryptography.X509Certificates.StoreLocation]
+$OpenFlags = [System.Security.Cryptography.X509Certificates.OpenFlags]
+$X509Certificate2  = [System.Security.Cryptography.X509Certificates.X509Certificate2]
+
+$cert1 = New-Object $X509Certificate2(Join-Path $PSScriptRoot 'ssl-1\root.cer')
+$cert2 = New-Object $X509Certificate2(Join-Path $PSScriptRoot 'ssl-2\root.cer')
+
+$store = New-Object $X509Store($StoreName::Root, $StoreLocation::CurrentUser)
+$store.Open($OpenFlags::ReadWrite)
+$store.Add($cert1)
+$store.Add($cert2)
+$store.Close()
